72be868cf71655deeb9524344b4603c1685e3837
Raphael <raphaelgarnier1997@gmail.com>
2025-03-30 13:24:58 +0200

    Creating scroling.js

---
+++ starter/scroling.js
@@ -0,0 +1,20 @@
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scroling
+  window.scrollTo(s1coords.left, s1coords.top);
+});
